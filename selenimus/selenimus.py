6800644dba25186293e561f134759734641faad4
Enma <enma@EnmanoMacBook-Pro.local>
2020-06-08 16:39:16 +0800

    delete DS_Store

--- selenimus/selenimus.py
+++ selenimus/selenimus.py
@@ -4,5 +4,9 @@ import os.path
 from selenium import webdriver
 
 from yamls import Selenimu_Yaml
+from logs import Loggingyaml
+
+logs=Loggingyaml().loggerin()
 
 Selenimu_Yaml().file_judge()
+logs.debug('debug')
